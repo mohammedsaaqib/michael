39117def07f702003f44d9c9d0444b439267133f
mohammedsaaqib <74182117+mohammedsaaqib@users.noreply.github.com>
2021-01-08 17:58:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas 
+var database;
+var gameState=0
+var playerCount=0;
+var form;
+var game; 
+var player
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    game=new Game();
+    game.getState();
+     game.start()  ; 
+    
+    
+}
+
+function draw(){
+
+}
+    
